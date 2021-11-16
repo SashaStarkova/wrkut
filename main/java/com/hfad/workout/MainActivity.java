0347b3e7be6c1cd833c6f16d7f0a6107626f41fa
SashaStark <84183933+SashaStarkova@users.noreply.github.com>
2021-11-16 20:28:22 +0500

    Add files via upload

---
+++ main/java/com/hfad/workout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hfad.workout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+        startActivity(intent);
+    }
+}
