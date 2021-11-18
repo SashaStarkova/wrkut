a2b144789b442112bbd3662a98431defa10b8955
SashaStark <84183933+SashaStarkova@users.noreply.github.com>
2021-11-18 22:03:16 +0500

    upd2
    18/11/21

--- main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -6,12 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.v4.app.FragmentTransaction;
+
 public class WorkoutDetailFragment extends Fragment {
     private long workoutId;
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        } else {
             workoutId = savedInstanceState.getLong("workoutId");
         }
     }
